088c1c785b31e3c9473a0e82e4def8bdc3d1ff0d
sama-aloda <alodas@mcmaster.ca>
2025-11-09 11:37:54 -0500

    Initial Commit

---
+++ WTMec/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WTMec
+//
+//  Created by Sama on 2025-11-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WTMec/WTMecApp.swift
@@ -0,0 +1,17 @@
+//
+//  WTMecApp.swift
+//  WTMec
+//
+//  Created by Sama on 2025-11-09.
+//
+
+import SwiftUI
+
+@main
+struct WTMecApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
